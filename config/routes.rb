5c554418a9d9fdd38d186aefe84258c1f6dc1769
Wes Griffin <typicalgames@gmail.com>
2019-08-19 17:34:52 +0000

    course error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :models
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
       resources :courses, only: [:new, :create, :show]
     end
