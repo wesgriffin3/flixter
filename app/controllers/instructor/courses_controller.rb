6f5355509f8b1fde9ba80bf604a8da659f9f5a98
Wes Griffin <typicalgames@gmail.com>
2019-08-24 00:52:30 +0000

    more details button broken

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
   def new
     @course = Course.new
   end
@@ -14,14 +16,23 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
-  end
-
-  
-  def course_params
-    params.require(:course).permit(:title, :description, :cost)
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != 
+      render plain "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 
 end
