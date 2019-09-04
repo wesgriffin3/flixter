8b4058153dd51c0dac42f5434c3a910e6b04cf13
Wes Griffin <typicalgames@gmail.com>
2019-09-04 22:57:29 +0000

    sections wont stick

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,15 +1,19 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
+  end
+
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
   end
 
   private
@@ -22,10 +26,14 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
     def current_course
+      if params[:course_id]
       @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
     end
+  end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
