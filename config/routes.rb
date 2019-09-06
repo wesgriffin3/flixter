c817350869dcb32417ede53384ffd8fe38261c35
Wes Griffin <typicalgames@gmail.com>
2019-09-06 02:49:27 +0000

    section form modal

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create, :update]
+      resources :sections, only: [:create, :update]
     end
   end
 end
