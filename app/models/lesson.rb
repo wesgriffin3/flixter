33901721c944a61d0002b806abb8436737ab92bb
Wes Griffin <typicalgames@gmail.com>
2019-09-07 16:34:48 +0000

    lesson modal bug

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,12 @@ class Lesson < ApplicationRecord
   belongs_to :section
   mount_uploader :video, VideoUploader
 
+
   include RankedModel
   ranks :row_order, with_same: :section_id
+
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    return lesson
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create, :update]
